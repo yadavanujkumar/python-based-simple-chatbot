ea7557bfb177df4e79e2856899843a0fa62ea07e
yadavanujkumar <86741869+yadavanujkumar@users.noreply.github.com>
2022-03-27 16:16:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+import re
+import long_responses as long
+
+
+def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
+    message_certainty = 0
+    has_required_words = True
+
+    # Counts how many words are present in each predefined message
+    for word in user_message:
+        if word in recognised_words:
+            message_certainty += 1
+
+    # Calculates the percent of recognised words in a user message
+    percentage = float(message_certainty) / float(len(recognised_words))
+
+    # Checks that the required words are in the string
+    for word in required_words:
+        if word not in user_message:
+            has_required_words = False
+            break
+
+    # Must either have the required words, or be a single response
+    if has_required_words or single_response:
+        return int(percentage * 100)
+    else:
+        return 0
+
+
+def check_all_messages(message):
+    highest_prob_list = {}
+
+    # Simplifies response creation / adds it to the dict
+    def response(bot_response, list_of_words, single_response=False, required_words=[]):
+        nonlocal highest_prob_list
+        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
+
+    # Responses -------------------------------------------------------------------------------------------------------
+    response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo' , 'wassup'], single_response=True)
+    response('See you!', ['bye', 'goodbye'], single_response=True)
+    response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
+    response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
+    response('Talking with you :-)', ['what', 'are', 'you', 'doing'], required_words=['what', 'doing'])
+    response('My name is Xeecup', ['what', 'is', 'your', 'name' , 'whats'], required_words=['what', 'name'])
+    response('Search it on internet', ['what', 'is', 'xeecup'], required_words=['what', 'xeecup'])
+    response('I am Xeecup the bot', ['who', 'are', 'you'], required_words=['who', 'you'])
+    response('I could have helped you with anything, but you should probably search it on internet', ['what', 'can', 'you' , 'do'], required_words=['what', 'do'])
+
+    # Longer responses
+    response(long.R_ADVICE, ['give', 'advice'], required_words=['advice'])
+    response(long.R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
+
+    best_match = max(highest_prob_list, key=highest_prob_list.get)
+   
+    return long.unknown() if highest_prob_list[best_match] < 1 else best_match
+
+
+# Used to get the response
+def get_response(user_input):
+    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
+    response = check_all_messages(split_message)
+    return response
+
+
+# the response system
+while True:
+    print('Xeecup: ' + get_response(input('You: ')))
